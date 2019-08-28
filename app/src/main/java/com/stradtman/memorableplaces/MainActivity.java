5c9a6da87faa067877a05b77b7dbc76773aefcd2
Kevin <stradtkt22@gmail.com>
2019-08-28 01:48:25 -0400

    Jumped to google maps activity

--- app/src/main/java/com/stradtman/memorableplaces/MainActivity.java
+++ app/src/main/java/com/stradtman/memorableplaces/MainActivity.java
@@ -2,7 +2,15 @@ package com.stradtman.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayList<String> places = new ArrayList<>();
+        places.add("Add a new place...");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, position, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+                startActivity(intent);
+            }
+        });
     }
 }
